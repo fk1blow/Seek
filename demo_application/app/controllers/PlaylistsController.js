e211f2b4a3b8bdc50953d0f1c8d17b93870e3799
fk1blow <hedphug@gmail.com>
2011-05-25 23:33:54 +0300

    Modified the demo app files

--- demo_application/app/controllers/PlaylistsController.js
+++ demo_application/app/controllers/PlaylistsController.js
@@ -1,53 +1,37 @@
-var ApplicationController = require('./ApplicationController');
+var AppController = Packagr.from('Controllers').load('ApplicationController');
 
 
- var PlaylistsController = Klass({
-	Extends: ApplicationController,
+var PlaylistsController = Klass({
+	Extends: AppController,
 	
 	methods: {
-		index: function() {
-			//this.renderHTML('no shitty worlds');
-			//console.log('x')
-			this.renderToLayout('playlists/index.html.ejs');
-			//cl('x')
-			//this.renderHTML('<div>mama are flori dar tata nu are</div>');
+		show: function() {
+			// Renders a view; Layout true
+			this.render('playlists/show.html', {
+				locals: {user: 'dragos', id: 443}
+			});
+			
+			// Renders a view; Layout false
+			//this.render('playlists/list.html');
+			
+			// Renders a sintrg; Layout false
+			//this.render({ what: 'de-te-n'});
+			
+			// Renders a sintrg; Layout true
+			//this.render({ what: 'de-te-n', layout: true});
+			
+			// Renders a string directly to response; the content-type is html by default and could be overwritten
+			//this.renderToResponse('<?xml version="1.0"?><note><to>Tove</to><</note>', 'xml');
 		},
 		
-		show: function() {
-			//this.renderToLayout('playlists/show.html.ejs', {
-			//	id: this.params.playlist_id
-			//});
-			
-			//Seek.URL.Response.sendToBody('im just a simple... ');
-			
-			//var s = 's'
-			
-			this.renderHTML('Hello worlds');
-			
-			//this.renderXML('<?xml version="1.0"?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don\'t forget me this weekend!</body></note>');
-			//this.renderJSON({
-			//	a: 'asa',
-			//	b: 'asa'
-			//});
-			
-			//throw new Error('asdas');
-			//cl('s')
-			
-			
-			//cl(s.s.s.s)
+		index: function() {
+			this.render('playlists/index.html', {
+				locals: {user: 'dragos', id: 443}
+			});
 		},
 		
 		search: function() {
 			//console.log('#search action');
-		},
-		
-		list_all: function() {
-			this.renderXML('<?xml version="1.0"?><note><to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Don\'t forget me this weekend!</body></note>');
-			//this.renderJSON({
-			//	a: 'asa',
-			//	b: 'asa'
-			//});
-			//this.renderToLayout('playlists/list.html.ejs');
 		}
 	}
 });
