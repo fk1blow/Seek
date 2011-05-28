491acb6f778171fa0708cb389fcd45b6aec99ec7
fk1blow <hedphug@gmail.com>
2011-05-29 02:24:58 +0300

    Modified demo_app environment file; added user defined configuration variables

--- demo_application/config/environment.js
+++ demo_application/config/environment.js
@@ -1,1 +1,16 @@
-Config.setEnvironment('development');
+var Config = Packagr.from('Config').load('Config');
+
+
+var Environment = {
+	state: 'development',
+	default_response: 'hahaha',
+	default_layout: 'alternative',
+	show_errors: true,
+	pages: {
+		404: true,
+		error: true
+	}
+}
+
+
+module.exports = Environment;
