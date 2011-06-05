85620f39e69126a3183bc2c5b4096a9a232dd233
fk1blow <hedphug@gmail.com>
2011-06-05 10:56:34 -0700

    Edited demo_application/config/routes.js via GitHub

--- demo_application/config/routes.js
+++ demo_application/config/routes.js
@@ -1,9 +1,17 @@
-ActiveRouter.Base.Router.map('/playlists/show/:playlist_id', { controller: 'playlists', action: 'show' });
+var Router = Packagr.from('Router').load('Base');
 
-ActiveRouter.Base.Router.map('/playlists', { controller: 'playlists', action: 'index' });
+// will map to PlaylistsController::show
+// params = [id, hidden]
+Router.Base.map("/playlists/show\?id=[0-9]+&hidden=[a-z]+", { controller: 'playlists', action: 'show' });
 
-ActiveRouter.Base.Router.map('/playlists/search', { controller: 'playlists', action: 'search' });
+// maps to PlaylistsController::show
+Router.Base.map('/playlists/show/:playlist_id', { controller: 'playlists', action: 'show' });
 
-ActiveRouter.Base.Router.map('/playlists/list', { controller: 'playlists', action: 'list_all' });
+// will map to PlaylistsController::index
+Router.Base.map('/playlists', { controller: 'playlists', action: 'index' });
 
-ActiveRouter.Base.Router.map('/:controller/:action');
+Router.Base.map('/playlists/search', { controller: 'playlists', action: 'search' });
+
+Router.Base.map('/playlists/list', { controller: 'playlists', action: 'list_all' });
+
+Router.Base.map('/users/index', { controller: 'users', action: 'index' });
